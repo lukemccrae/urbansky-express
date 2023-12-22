24bcaf7f252c1717b0717a30bf98ed8e3690f7d3
lukemccrae <lukemccrae@gmail.com>
2023-12-22 12:19:48 -0700

    add put route

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,31 @@ router.get('/products', async function(req, res, next) {
   res.json(result.rows)
 });
 
+router.put('/products/:id', async (req, res) => {
+  const productId = req.params.id;
+  const { serial_number, product_name, description, quantity, created_at } = req.body;
+
+  const updateQuery = `
+    UPDATE products
+    SET serial_number = $1, product_name = $2, description = $3, quantity = $4, created_at = $5
+    WHERE id = $6
+    RETURNING *;`;
+
+  const values = [serial_number, product_name, description, quantity, created_at, productId];
+
+  try {
+    const result = await pool.query(updateQuery, values);
+    if (result.rowCount > 0) {
+      res.json({ message: `Product with ID ${productId} updated successfully`, updatedData: result.rows[0] });
+    } else {
+      res.status(404).json({ message: `Product with ID ${productId} not found` });
+    }
+  } catch (error) {
+    console.error('Error updating product:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 router.post('/products', async function(req, res, next) {
   console.log(req.body,'<< req body')
 
